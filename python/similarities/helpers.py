28cf11c0c51f18558d66241227436b889267f67a
bl0rch1d <temaflash96@gmail.com>
2020-01-22 22:49:04 +0200

    Done

--- python/similarities/helpers.py
+++ python/similarities/helpers.py
@@ -1,19 +1,30 @@
+from nltk.tokenize import sent_tokenize
+
+
 def lines(a, b):
     """Return lines in both a and b"""
 
-    # TODO
-    return []
+    a, b = a.splitlines(), b.splitlines()
+
+    return compare_lists(a, b)
 
 
 def sentences(a, b):
     """Return sentences in both a and b"""
 
-    # TODO
-    return []
+    a, b = sent_tokenize(a), sent_tokenize(b)
+
+    return compare_lists(a, b)
 
 
 def substrings(a, b, n):
     """Return substrings of length n in both a and b"""
 
-    # TODO
-    return []
+    result_a = [a[i:i+n] for i in range(len(a))]
+    result_b = [b[i:i+n] for i in range(len(b))]
+
+    return compare_lists(result_a, result_b)
+
+
+def compare_lists(list_1, list_2):
+    return list(set(list_1) & set(list_2))
